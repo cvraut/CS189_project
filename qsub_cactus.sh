16d6ba2087f049b2326ce54cf7f6065fc04a5e28
Chinmay Viraj Raut <craut@hpc-interactive-1-1.local>
2019-04-29 15:57:59 -0700

    take 2 for satsuma & cactus scripts

--- qsub_cactus.sh
+++ qsub_cactus.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 #
-#$ -N cactusAl_yeast
-### -t 1-1
-#$ -q pub8i
+#$ -N cactus_yeast_script
+#$ -t 1        ## task ids
+#$ -q pub8i      ## queue to send task to
+#$ -ckpt restart ## include to make sure tasks are suspended & checkpointed
 ### -m beas
 ### -M craut@uci.edu
 ### -o
 ### -e
-#$ -pe openmp 16
+#$ -pe openmp 1  ## multiple nodes, integer is # of cores
 
+source ~/.miniconda3rc
+cd /pub/{$USER}/CS189_project
+conda --version
+conda activate cg_yeast
+## cactus -h
+cd jobStore
+rm -rf *.*
+cd ..
 cactus jobStore yeast.txt outputs/yeast.hal --binariesMode local
+
+conda deactivate
---
+++ qsub_satsuma.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+#$ -N satsuma_yeast_script
+#$ -t 1-4        ## task ids
+#$ -q pub8i      ## queue to send task to
+## -ckpt restart ## include to make sure tasks are suspended & restart. Only for use in suspendable queues.
+### -m beas
+### -M craut@uci.edu
+### -o
+### -e
+#$ -pe openmp 1  ## for multithreaded applications. pe openmp INT, integer is # of cores
+
+source ~/.miniconda3rc
+cd /pub/{$USER}/CS189_project
+conda --version
+conda activate cg_yeast
+## cactus -h
+## SatsumaSynteny2 -h
+cd outputs
+rm -rf *.*
+cd ..
+echo "Running Satsuma"
+export SATSUMA2_PATH=~/bin/miniconda3/envs/cg_yeast/bin/
+## run satsuma
+#mkdir ./outputs/suvarum_cbs7001
+#SatsumaSynteny2 -q data/g833-1B_reference.fasta -t data/suvarum_cbs7001.fasta -o outputs/suvarum_cbs7001
+#mkdir ./outputs/S288C_reference
+#SatsumaSynteny2 -q data/g833-1B_reference.fasta -t data/S288C_reference.fasta -o outputs/S288C_reference
+#mkdir ./outputs/SK1_reference
+#SatsumaSynteny2 -q data/g833-1B_reference.fasta -t data/SK1_reference.fasta -o outputs/SK1_reference
+#mkdir ./outputs/W303_reference
+#SatsumaSynteny2 -q data/g833-1B_reference.fasta -t data/W303_reference.fasta -o outputs/W303_reference
+
+
+SEED=(head -n ${SGE_ARRAY_TASK_ID} $JOBFILE | tail -n 1)
+rm -rf ./outputs/${SEED}
+mkdir -p ./outputs/${SEED}
+SatsumaSynteny2 -q data/g833-1B_reference.fasta -t data/${SEED}.fasta -o outputs/${SEED}
+
+conda deactivate
