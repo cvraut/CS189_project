6b7c4b41b64555ea7b1fe7d955c56ef9b0085ecb
Beoung Hun Lee <beoungl@hpc-interactive-1-1.local>
2019-05-30 00:46:57 -0700

    NUCMER CODE

---
+++ qsub_nucmer.sh
@@ -0,0 +1,35 @@
+
+#!/bin/bash
+#
+#$ -N nucmer
+#$ -t 1-4
+#$ -q pub8i
+## ckpt restart
+### -m beas
+### -M beoungl@uci.edu
+### -o
+### -e
+#$ -pe openmp 8-32
+
+source /data/users/beoungl/bin/.miniconda3testrc
+cd /pub/${USER}/CS189_backup/data
+conda --version
+conda activate project1
+
+echo "Running nucmer"
+
+REF=$(cat nucmer.ls | head -n 1)
+SEED=$(cat nucmer.ls | head -n $((${SGE_TASK_ID}+1)) | tail -n 1)
+##SEED=${SEED%%.*}
+
+
+echo "$((${SGE_TASK+ID}+1))"
+echo "$(REF)"
+echo "${SEED}"
+
+nucmer --maxmatch -c 100 -p ${SEED} ${REF} ${SEED}
+mummerplot -f --filter ${SEED}.delta -R ${REF} -Q ${SEED}
+
+conda deactivate
+
+
