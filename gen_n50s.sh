630244128b57b52f53bd6074c930674c92a0979f
Chinmay Viraj Raut <craut@hpc-interactive-1-1.local>
2019-06-14 20:08:19 -0700

    assembly stats bash script

---
+++ gen_n50s.sh
@@ -0,0 +1,10 @@
+source ~/.miniconda3rc
+conda activate cg_yeast
+cd /pub/craut/CS189_project
+mkdir assembly_stats
+assembly-stats data/SK1_reference.fasta > assembly_stats/SK1_reference.stats
+assembly-stats data/S288C_reference.fasta > assembly_stats/S288C_reference.stats
+assembly-stats data/suvarum_cbs7001.fasta > assembly_stats/suvarum_cbs7001.stats
+assembly-stats data/g833-1B_reference.fasta > assembly_stats/g833-1B_reference.stats
+assembly-stats data/W303_reference.fasta > assembly_stats/W303_reference.stats
+conda deactivate
